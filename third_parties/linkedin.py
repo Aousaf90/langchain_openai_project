85801be1c3a136e84176fc35690304a61b6247e4
Aousaf Sulaman <59832341+Aousaf90@users.noreply.github.com>
2024-07-22 00:12:43 +0500

    first commit

---
+++ third_parties/linkedin.py
@@ -0,0 +1,11 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+
+def scrap_linked_profile(linked_in_profile:str, mock:bool = False):
+    """ 
+    Scrap information from linkedin profile
+    Manually scrap the inforamation from the linkedin profile
+    """
+    pass
